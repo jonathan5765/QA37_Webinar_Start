2e103ce09295bf08d0a83e65131f1fcf6b202514
jonathan5765 <jonathan5765@gmail.com>
2023-03-11 21:25:42 +0200

    Chrome Driver upgrade

--- src/test/java/Start.java
+++ src/test/java/Start.java
@@ -1,14 +1,30 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class Start {
 
     WebDriver wd;
 
+    @BeforeClass
+    public void setUp(){
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+
+        wd = new ChromeDriver(options);
+
+
+        //wd.get("https://google.com/");
+        //wd.get("https://trello.com/");
+    }
+
     @Test
     public void start1(){
-        wd = new ChromeDriver();
+        //wd = new ChromeDriver();
         wd.navigate().to("https://google.com/");
         wd.navigate().back();
         wd.navigate().forward();
@@ -24,7 +40,7 @@ public class Start {
 
     public void start2(){
 
-        wd=new ChromeDriver();
+        //wd=new ChromeDriver();
 
         wd.navigate().to("https://trello.com/");
 
